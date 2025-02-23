0d211617b373507c4633e6273e6ff035e062c1b6
Lithyem, inc. <michael@lithyem.net>
2025-02-23 09:17:50 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def sanitize_text(text):
 	Remove BOM, unwanted control characters, and Unicode line/paragraph separators.
 	"""
 	text = text.replace("<", "&lt;").replace(">", "&gt;")
- 	text = text.lstrip('\ufeff')
+	text = text.lstrip('\ufeff')
 	text = re.sub(r'[\x00-\x08\x0B-\x0C\x0E-\x1F]', '', text)
 	text = re.sub(u'[\u2028\u2029]', '', text)
 	return text
